82cd532df593bafe5322b63c5c77a44a86a3858c
Corentin Rivot <corentin.rivot@epitech.eu>
2013-11-12 13:11:46 +0100

    Add extern declaration of mysql function

--- src/mysql.rs
+++ src/mysql.rs
@@ -1,6 +1,6 @@
 use mysql_com::NET;
-use mysql_ot::{LIST, MEM_ROOT, st_mysql_options};
-use std::libc::{c_uint, c_void, c_schar, c_ulong, c_uchar, c_ulonglong};
+use mysql_ot::{LIST, MEM_ROOT, st_mysql_options, enum_mysql_option};
+use std::libc::{c_uint, c_void, c_schar, c_ulong, c_uchar, c_ulonglong, c_int};
 use std::ptr;
 
 type c_bool = c_schar;
@@ -93,6 +93,51 @@ extern "C" {
                           db: *c_schar, port: c_uint,
                           unix_socket: *c_schar,client_flag: c_ulong)
                           -> *mut MYSQL;
+    fn mysql_close(mysql: *mut MYSQL) -> ();
+    fn mysql_affected_rows(mysql: *mut MYSQL) -> c_ulonglong;
+    fn mysql_autocommit(mysql: *mut MYSQL, mode: c_bool) -> c_bool;
+    fn mysql_change_user(mysql: *mut MYSQL, user: *c_schar,
+                         passwd: *c_schar, db: *c_schar) -> c_bool;
+    fn mysql_character_set_name(mysql: *mut MYSQL) -> *c_schar;
+    fn mysql_commit(mysql: *mut MYSQL) -> c_bool;
+    fn mysql_debug(debug: *c_schar);
+    fn mysql_dump_debug_info(mysql: *mut MYSQL) -> c_int;
+    fn mysql_errno(mysql: *mut MYSQL) -> c_uint;
+    fn mysql_error(mysql: *mut MYSQL) -> *c_schar;
+    fn mysql_field_count(mysql: *mut MYSQL) -> c_uint;
+    fn mysql_get_client_info() -> *c_schar;
+    fn mysql_get_client_version() -> c_ulong;
+    fn mysql_get_host_info(mysql: *mut MYSQL) -> *c_schar;
+    fn mysql_get_proto_info(mysql: *mut MYSQL) -> c_uint;
+    fn mysql_get_server_info(mysql: *mut MYSQL) -> *c_schar;
+    fn mysql_get_server_version(mysql: *mut MYSQL) -> c_ulong;
+    fn mysql_get_ssl_cipher(mysql: *mut MYSQL) -> *c_schar;
+    fn mysql_hex_string(to: *mut c_schar, from: *c_schar,
+                        from_length: c_ulong) -> c_ulong;
+    fn mysql_info(mysql: *mut MYSQL) -> *c_schar;
+    fn mysql_insert_id(mysql: *mut MYSQL) -> c_ulonglong;
+    fn mysql_kill(mysql: *mut MYSQL, pid: c_ulong) -> c_int;
+    fn mysql_server_end();
+    fn mysql_library_end();
+    fn mysql_more_results(mysql: *mut MYSQL) -> c_bool;
+    fn mysql_next_result(mysql: *mut MYSQL) -> c_int;
+    fn mysql_options(mysql: *mut MYSQL, option: enum_mysql_option,
+                     arg: *c_void) -> c_int;
+    fn mysql_ping(mysql: *mut MYSQL) -> c_int;
+    fn mysql_query(mysql: *mut MYSQL, q: *c_schar) -> c_int;
+    fn mysql_real_escape_string(mysql: *mut MYSQL, to: *mut c_schar,
+                                from: *c_schar, length: c_ulong) -> c_ulong;
+    fn mysql_real_query(mysql: *mut MYSQL, q: *c_schar, length: c_ulong)
+                        -> c_int;
+    fn mysql_refresh(mysql: *mut MYSQL, refresh_options: c_uint) -> c_int;
+    fn mysql_rollback(mysql: *mut MYSQL) -> c_bool;
+    fn mysql_select_db(mysql: *mut MYSQL, db: *c_schar) -> c_int;
+    fn mysql_set_character_set(mysql: *mut MYSQL, csname: *c_schar) -> c_int;
+    fn mysql_set_local_infile_default(mysql: *mut MYSQL);
+    fn mysql_sqlstate(mysql: *mut MYSQL) -> *c_schar;
+    fn mysql_ssl_set(mysql: *mut MYSQL, key: *c_schar, cert: *c_schar,
+                     ca: *c_schar, capath: *c_schar, cipher: *c_schar) -> c_bool;
+    fn mysql_stat(mysql: *mut MYSQL) -> *c_schar;
 }
 
 #[fixed_stack_segment]
@@ -120,5 +165,8 @@ pub fn real_connect(mut mysql: *mut MYSQL, host: &str, user: &str, passwd: &str,
     return Some(mysql);
 }
 
-
+#[fixed_stack_segment]
+pub fn close(mysql: *mut MYSQL) -> () {
+    unsafe { mysql_close(mysql); }
+}
 
